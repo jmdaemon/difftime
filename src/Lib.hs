5c0aa8e199f39ba4dc5ca457304b2638b60d12a9
Joseph Diza <josephm.diza@gmail.com>
2022-03-05 21:04:38 -0800

    Add split functions

--- src/Lib.hs
+++ src/Lib.hs
@@ -2,5 +2,27 @@ module Lib
     ( someFunc
     ) where
 
+import Text.Printf
+
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
+
+
+split :: String -> String -> [String]
+split _ "" = []
+split delim str =
+  split' "" str []
+  where
+    dl = length delim
+
+    split' :: String -> String -> [String] -> [String]
+    split' h t f
+      | dl > length t = f ++ [h ++ t]
+      | delim == take dl t = split' "" (drop dl t) (f ++ [h])
+      | otherwise = split' (h ++ take 1 t) (drop 1 t) f
+
+splitColon string = split ":"
+splitHypen string = split "-"
+
+splitAM string = split "am"
+splitPM string = split "pm"
