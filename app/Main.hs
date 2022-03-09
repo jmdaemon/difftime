9eb667b35757d8179b1e76d1346c150009d8b82e
Joseph Diza <josephm.diza@gmail.com>
2022-03-08 18:22:30 -0800

    Remove obsolete code

--- app/Main.hs
+++ app/Main.hs
@@ -11,42 +11,11 @@ data Sample = Sample
   { t1 :: String
   , t2 :: String }
 
---replicateString :: Sample -> IO ()
---replicateString (Sample string n flip) = 
-    --do 
-      --if not flip then putStrLn repstring else putStrLn $ reverse repstring
-          --where repstring = foldr (++) "" $ replicate n string
---parse :: Sample -> IO ()
---parse :: Sample -> IO () -> String
---parse :: Sample -> IO() -> String
+-- We cannot directly return a String from an impure function
+-- so we just print out the result to stdout
 parse :: Sample -> IO()
 parse (Sample t1 t2) =
         putStrLn $ toDiffTime t1 t2
-        --putStrLn t1
-        --putStrLn (read t1 :: String)
-        --putStrLn (read t1 :: String) (read t2 :: String)
-        --putStrLn (diffTime (read t1 :: String) (read t2 :: String))
-        --putStrLn $ diffTime do(t1 t2)
-        --putStrLn $ do (diffTime <- t1 t2)
-    --putStrLn $ t1 t2 >>= diffTime
-    --putStrLn $ diffTime <- t1 t2
-    --diffTime >>= t1 t2
-    --do
-        --diffTime $ replicate <- t1 t2
-        --replicate t1 t2 >>= diffTime t1 t2
-        --diffTime $ replicate t1 t2
-        --diffTime $ putStrLn repstring
-        --diffTime repstring
-            --where repstring = replicate t1 t2
-            --where repstring = foldr (++) "" $ replicate t1 t2
-
-    --do
-        --if not flip then putStrLn repstring
-                    --else putStrLn $ reverse repstring
-          --where repstring = foldr (++) "" $ replicate n string
-    --putStr $ toDiffTime t1 t2
-    --toDiffTime t1 t2
-    --putStrLn $ toDiffTime t1 t2
 
 sample :: Parser Sample
 sample = Sample
@@ -65,34 +34,14 @@ main = execParser opts >>= parse
      <> progDesc "Calculate the difference of a time interval"
      <> header "difftime - Calculate time differences of intervals" )
 
---main :: IO ()
-
---main = someFunc
-
---main = do
-    --args <- getArgs
-    --putStrLn $ head args
-
---main = do
-    --args <- getArgs
-    --putStrLn toDiffTime
-
--- diffTime "10:30pm" "9:00pm"
 toDiffTime :: String -> String -> String
 --toDiffTime :: [String] -> String
 --toDiffTime [s1, s2] = 
 --toDiffTime s1 s2 = 
     ----let t1 = head s1
         ----t2 = concat (tail s2)
-    --let t1 = read (head s1) :: String
-        --t2 = read (concat (tail s2)) :: String
-     --in diffTime t1 t2
 toDiffTime s1 s2 = 
     --let t1 = head s1
         --t2 = concat (tail s2)
         diffTime s1 s2
 
---usage   = putStrLn "Usage: difftime [TIME] [TIME]"
---version = putStrLn "difftime v0.1.0"
---exit    = exitWith ExitSuccess
---die     = exitWith (ExitFailure 1)
