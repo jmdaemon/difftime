80e140fefd1bc059f883d3fac8119970783a2dd9
Joseph Diza <josephm.diza@gmail.com>
2022-03-25 13:45:12 -0700

    Make diffTime use new Interval & Time data classes

--- src/DiffTimeLib.hs
+++ src/DiffTimeLib.hs
@@ -37,9 +37,12 @@ data Time = Time { hours :: Int
                  , seconds :: Int
                  }
 
--- | Calculate the difference between two times
-diffTime :: Time -> Time -> [Int]
-diffTime (Time h_f m_f s_f) (Time h_i m_i s_i) = [diff h_f h_i, diff m_f m_i, diff s_f s_i]
+-- | Interval | --
+data Interval = Interval { initial :: Time
+                         , final :: Time
+                         }
+
+-- | Time Data Functions | --
 
 -- | Sum two times together
 sumTime :: Time -> Time -> Time
@@ -52,21 +55,22 @@ showTime (Time h m s) = printf "%d hours %d mins %d secs" h m s
 mkTime :: Int -> Int -> Int -> Time
 mkTime h m s = Time h m s
 
--- | Interval | --
-data Interval = Interval { t_i :: Time
-                         , t_f :: Time
-                         }
+-- | Interval Data Functions | --
+
+-- | Calculate the difference in time over a time interval
+diffInterval :: Interval -> Time
+diffInterval (Interval t_f t_i) = Time (hours t_f - hours t_i) (minutes t_f - minutes t_i) (seconds t_f - seconds t_i)
 
 -- | Calculates the difference between two times and show their difference
 diffTimeHourMin :: String -> String -> String
 diffTimeHourMin t1 t2 =
     let [h1,m1] = mkTimeList t1
         [h2,m2] = mkTimeList t2
-    in showTime (diffTime (mkTime h2 m2 0) (mkTime h1 m1 0))
+    in showTime (diffInterval (mkTime h2 m2 0) (mkTime h1 m1 0))
 
     --let [h1,m1,s1] = mkTimeList t1
         --[h2,m2,s2] = mkTimeList t2
-    --in showTime (diffTime (mkTime h2 m2 s2) (mkTime h1 m1 s1))
+    --in showTime (diffInterval (mkTime h2 m2 s2) (mkTime h1 m1 s1))
 
 -- Pad adds zeros to the time
 padTime :: String -> String -> [Int]
