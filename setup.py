182bff5de57b30f998b0025b0f275fa6b75953ed
Joseph Diza <josephm.diza@gmail.com>
2022-05-09 02:28:57 -0700

    Add program version

--- setup.py
+++ setup.py
@@ -1,4 +1,6 @@
 from setuptools import find_packages, setup
+import codecs
+import os.path
 
 with open('README.md', 'r', encoding='utf-8') as fh:
     long_description = fh.read()
@@ -10,8 +12,23 @@ def get_data_files():
 
     return data_files
 
+def read(rel_path):
+    here = os.path.abspath(os.path.dirname(__file__))
+    with codecs.open(os.path.join(here, rel_path), 'r') as fp:
+        return fp.read()
+
+def get_version(rel_path):
+    ''' Single source package version in src/package_name/__init__.py '''
+    for line in read(rel_path).splitlines():
+        if line.startswith('__version__'):
+            delim = '"' if '"' in line else "'"
+            return line.split(delim)[1]
+    else:
+        raise RuntimeError("Unable to find version string.")
+
 setup(
     name='difftime',
+    version=get_version("src/sapply/__init__.py"),
     license='GPL-3.0',
     author='Joseph Diza',
     author_email='josephm.diza@gmail.com',
