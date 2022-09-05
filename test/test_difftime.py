518f45797c818bc82c610d13c42bca57cd5b55a1
Joseph Diza <josephm.diza@gmail.com>
2022-09-05 13:30:56 -0700

    Refactor unit tests for convert_24_hour

--- test/test_difftime.py
+++ test/test_difftime.py
@@ -1,52 +1,38 @@
-from difftime.time import (
-    convert_24_hour,
-    to_time,
-)
+from difftime.time import convert_24_hour
+
+# Helper functions
+def run_test_24hr(expect):
+    actual = convert_24_hour(expect, False)
+    assert(actual == expect)
+
+def run_test_12hr(time, expect):
+    actual = convert_24_hour(time, False)
+    assert(actual == expect)
 
 # convert_24_hour tests
 
 # 24 Hour
 def test_24hour_am():
-    expect = "7:00"
-    actual = convert_24_hour(expect, False)
-    assert(actual == expect)
+    run_test_24hr("7:00")
 
 def test_24hour_pm():
-    expect = "21:00"
-    actual = convert_24_hour(expect, False)
-    assert(actual == expect)
+    run_test_24hr("21:00")
 
 def test_24hour_noon():
-    expect = "12:00"
-    actual = convert_24_hour(expect, False)
-    assert(actual == expect)
+    run_test_24hr("12:00")
 
 def test_24hour_midnight():
-    expect = "00:00"
-    actual = convert_24_hour(expect, False)
-    assert(actual == expect)
+    run_test_24hr("00:00")
 
 # 12 Hour
 def test_12hour_am():
-    time = "7:00am"
-    expect = "7:00"
-    actual = convert_24_hour(time, False)
-    assert(actual == expect)
+    run_test_12hr("7:00am", "7:00")
 
 def test_12hour_pm():
-    time = "9:00pm"
-    expect = "21:00"
-    actual = convert_24_hour(time, False)
-    assert(actual == expect)
+    run_test_12hr("9:00pm", "21:00")
 
 def test_12hour_noon():
-    time = "12:00pm"
-    expect = "12:00"
-    actual = convert_24_hour(time, False)
-    assert(actual == expect)
+    run_test_12hr("12:00pm", "12:00")
 
 def test_12hour_midnight():
-    time = "12:00am"
-    expect = "0:00"
-    actual = convert_24_hour(time, False)
-    assert(actual == expect)
+    run_test_12hr("12:00am", "0:00")
